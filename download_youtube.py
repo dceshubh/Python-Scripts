e7e412c22f2a08a73cc19d604e750256b3144128
dceshubh <shubham94.varshney@gmail.com>
2016-01-27 16:44:28 +0530

    upadted the youtube download code ..options are now dispalyed to the user

--- download_youtube.py
+++ download_youtube.py
@@ -1,10 +1,11 @@
 import urllib.request, os
 from bs4 import BeautifulSoup
 import re
+from tabulate import tabulate
 
 
 def get_links(url):
-    print(" ------- Fetching HTML of url: %s ------ "  %url)
+    print(" ------- Fetching HTML of url: %s ------ " % url)
     request = urllib.request.urlopen(url)
     a = request.read()
     print(" ---------- Got HTML ----------- ")
@@ -17,8 +18,12 @@ def get_links(url):
     links = list()
     for link in links_h:
         links.append(link.find('a'))
+
+    durations = list()
+    for link in links_h:
+        durations.append(link.find('span'))
         
-    return links
+    return links,durations
         
  
 def download_video(url):
@@ -52,47 +57,56 @@ def start_function():
     f = open(fname, "r")
     shows = f.readlines()
     f.close()
-    new_list = list()
+    new_list_shows = list()
     for show in shows:
         print(" ------------ PROCESSING SHOW %s ------------- " % show)
-        base_url = "https://www.youtube.com/results?q="
-        query = show
-        rest_url = urllib.parse.quote(query)
-        url = base_url + rest_url
-        links = get_links(url)
-        final_link = None
-        try:
-            episode_number = re.search(r'(?s)episode(.*)', query, re.IGNORECASE).group(0)
-            for link in links:
-                link_details = link.string
-                link_details = link_details.lower()
-                if episode_number in link_details:
-                    final_link = link['href']
-                    break
+        episode_number = re.search(r'(?s)episode(.*)', show.split("||")[0], re.I).group(0)
+        if show.split("||")[-1].startswith("http") is not True:
+            base_url = "https://www.youtube.com/results?q="
+            query = show
+            rest_url = urllib.parse.quote(query)
+            url = base_url + rest_url
+            links, durations = get_links(url)
+            table = list()
+            for i, link in enumerate(links):
+                new_list = list()
+                new_list.append(i+1)
+                new_list.append(link.string.lower())
+                new_list.append(durations[i].string)
+                table.append(new_list)
 
-            if final_link is not None:
-                final_link = "https://www.youtube.com" + final_link
-                val = download_video(final_link)
-                if val is not 0:
-                    new_list.append(show)
+            print(tabulate(table, headers=["S.NO", "TITLE", "DURATION"], tablefmt="fancy_grid"))
+            if len(links) >= 1:
+                ans = int(input(" ------- Select any value between 1 and %s (both inclusive) to download the corresponding video or select any value greater than %s if donot want to download any video" % (len(links), len(links))))
+                if ans > len(links):
+                    print(" ------------- No Video Selected ----------------- ")
+                    new_list_shows.append(show)
                 else:
-                    new_list.append(get_next_episode_show(show, episode_number))
+                    final_link = links[ans-1]['href']
+                    final_link = "https://www.youtube.com" + final_link
+                    new_list_shows.append(show + "||" + final_link)
+                    val = download_video(final_link)
+                    if val is 0:
+                        new_list_shows.pop()
+                        new_list_shows.append(get_next_episode_show(show, episode_number))
 
-        except:
-            print(" ------ No Video Available according to the query ------")
-            new_list.append(show)
-
-        if final_link is None:
-            print(" ------ No Video Available according to the query ------")
-            new_list.append(show)
+            else:
+                print(" ------ No Video Available according to the query ------")
+                new_list_shows.append(show)
+        else:
+            final_link = show.split("||")[-1]
+            new_list_shows.append(show)
+            val = download_video(final_link)
+            if val is 0:
+                new_list_shows.pop()
+                new_list_shows.append(get_next_episode_show(show.split("||")[0], episode_number))
 
     f = open(fname, "w")
-    for show in new_list:
+    for show in new_list_shows:
         f.write(show)
-        f.write("\n")
 
     print(" ----------------- UPDATED LIST -----------------")
-    print(new_list)
+    print(new_list_shows)
     f.close()
 
 
